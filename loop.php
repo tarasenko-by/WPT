14ddb5738b97a8ef4d81facf3ed3f4bb74fc0f73
DESKTOP-ORP2DGR\Tarasenko <tarasenko-by@mail.ru>
2017-08-15 13:06:51 +0300

    Подправил типографику и в loop.php убрал комментарии

--- loop.php
+++ loop.php
@@ -6,7 +6,7 @@
  */ 
 ?>
 
-<article id="post-<?php the_ID(); ?>" <?php post_class('story row pmn'); ?>> <?php // контэйнер с классами и id ?>
+<article id="post-<?php the_ID(); ?>" <?php post_class('story row pmn'); ?>>
 	<?php if ( has_post_thumbnail() ) { ?>
 			<div class="col-sm-5 pmn">
 				<a href="<?php the_permalink(); ?>">
@@ -14,19 +14,19 @@
 				</a>
 			</div>
 		<?php } ?>
-	<div class="<?php if ( has_post_thumbnail() ) { ?>col-sm-7<?php } else { ?>col-sm-12<?php } // разные классы в зависимости есть ли миниатюра ?>">
+	<div class="<?php if ( has_post_thumbnail() ) { ?>col-sm-7<?php } else { ?>col-sm-12<?php }?>">
 
 		<h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
 
-		<?php the_content(''); // пост превью, до more ?>
+		<?php the_content('');?>
 
 		<div class="info">
 				<div class="date">
-					<p><?php the_time(get_option('date_format')." в ".get_option('time_format')); ?></p> <?php // дата и время создания ?>
+					<p><?php the_time(get_option('date_format')." в ".get_option('time_format')); ?></p>
 				</div>
 
 			<div class="category text-right">
-				<svg class="icon icon-category"><use xlink:href="#icon-category"></use></svg> <?php the_category(',') ?><?php // ссылки на категории в которых опубликован пост, через зпт ?>
+				<svg class="icon icon-category"><use xlink:href="#icon-category"></use></svg> <?php the_category(',') ?>
 			</div>
 
 			<div class="clearfix"></div>
