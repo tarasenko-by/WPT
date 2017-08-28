a1528c3c43e858e2dbefa887167e73accb8ffc34
DESKTOP-J512ESH\Tarasenko <tarasenko-by@mail.ru>
2017-08-29 00:24:49 +0300

    Подправил календарь. Сделал page-custom.php

--- page-custom.php
+++ page-custom.php
@@ -5,19 +5,34 @@
  * @subpackage Antares
  * Template Name: Custom Tamplate
  */
-get_header();?>
+
+get_header(); ?>
+
 <section>
 	<div class="container">
 		<div class="row">
-			<div class="<?php content_class_by_sidebar(); ?>">
-				<?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
-					<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>> 
-						<h1><?php the_title();?></h1>
-						<?php the_content();?>
-					</article>
-				<?php endwhile;?>
+
+
+			<div class="col-sm-12 content">
+
+				<div class="slide">
+					<h1 class="pmn title text-center"><?php the_title();?></h1>
+					<ul class="breadcrumb">
+						<?php the_breadcrumb() ?>
+					</ul>
+				</div>
+
+				<div class="content-block">
+					<div class="content-wrapper">
+						<?php if ( have_posts() ) while ( have_posts() ) : the_post();?>
+							<article id="post-<?php the_ID(); ?>" <?php post_class('block page'); ?>> 
+								<?php the_content();?>
+							</article>
+						<?php endwhile;?>
+					</div>
+				</div>
 			</div>
-			<?php get_sidebar();?>
+
 		</div>
 	</div>
 </section>
